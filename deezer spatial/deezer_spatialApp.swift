15e57bc7af684347ead4b9a224a91aa6e4814e74
Calvin <perrot.calvin@outlook.fr>
2023-01-11 15:14:40 +0100

    Initial Commit

---
+++ deezer spatial/deezer_spatialApp.swift
@@ -0,0 +1,17 @@
+//
+//  deezer_spatialApp.swift
+//  deezer spatial
+//
+//  Created by Fabien Kattic on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct deezer_spatialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
